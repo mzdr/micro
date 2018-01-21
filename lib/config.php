82a553295698d0ba49d71888406cdc6b8cef3403
Sebastian Prein <hi@sebastianprein.com>
2018-01-21 20:23:37 +0100

    Drop “adbario/php-dot-notation” in favor of “samrap/gestalt”

--- lib/config.php
+++ lib/config.php
@@ -2,16 +2,75 @@
 
 namespace µ;
 
-use \Adbar\Dot;
+use DirectoryIterator;
+use Gestalt\Configuration;
+use SplFileInfo;
+use Symfony\Component\Yaml\Yaml;
 
-function config() {
+/**
+ * Provides access to the Gestalt configuration package.
+ *
+ * @param array ...$args
+ * @return \Gestalt\Configuration|mixed
+ * @see https://github.com/samrap/gestalt
+ */
+function config(...$args) {
     static $config;
 
-    if ($config instanceof Dot === false) {
-        $config = new Dot;
+    if ($config instanceof Configuration === false) {
+        $config = new class extends Configuration {
+
+            /**
+             * Appends a single configuration file or all files
+             * from a given directory into the current configuration.
+             *
+             * @param $path Path of configuration file or directory.
+             * @param string $preferredExtension Preferred files to use when scanning directory. Defaults to `php`.
+             * @return self
+             */
+            public function append($path, string $preferredExtension = 'php'): self {
+                $loaders = [
+                    'ini' => function($file) { return parse_ini_file($file, true); },
+                    'json' => function($file) { return json_decode(file_get_contents($file)); },
+                    'php' => function($file) { return require $file; },
+                    'yaml' => function($file) { return Yaml::parseFile($file); }
+                ];
+
+                $resource = $path instanceof SplFileInfo ? $path : new SplFileInfo($path);
+                $extension = $resource->getExtension();
+
+                if ($resource->isFile() && isset($loaders[$extension])) {
+                    $loader = $loaders[$extension];
+                    $append = new static;
+
+                    foreach ($loader($resource->getPathname()) as $key => $value) {
+                        $append->set($key, $value);
+                    }
+
+                    $append = $append->all();
+                    $this->original = array_merge($this->original, $append);
+                    $this->items = array_merge($this->items, $append);
+
+                    $this->notify();
+
+                    return $this;
+                }
+
+                if ($resource->isDir()) {
+                    $directory = new DirectoryIterator(realpath($path));
+
+                    foreach ($directory as $file) {
+                        if ($file->isFile() && $file->getExtension() === $preferredExtension) {
+                            $this->append($file);
+                        }
+                    }
+                }
+
+                return $this;
+            }
+        };
     }
 
-    $args = func_get_args();
     $count = count($args);
 
     if ($count === 0) {
@@ -19,9 +78,9 @@ function config() {
     }
 
     if ($count === 2) {
-        $config[$args[0]] = $args[1];
+        $config->set(...$args);
 
-        return;
+        return $config;
     }
 
     if (is_array($args[0])) {
@@ -29,7 +88,7 @@ function config() {
             config($key, $value);
         }
 
-        return;
+        return $config;
     }
 
     return $config[$args[0]];
