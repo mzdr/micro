df6177119861bafa46f63a88c61766f05158c7a6
Sebastian Prein <hi@sebastianprein.com>
2018-01-21 21:25:28 +0100

    Add error handler “BooBoo”

---
+++ lib/error.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace µ;
+
+use League\BooBoo\BooBoo;
+use Gestalt\Util\ObserverInterface;
+use Gestalt\Util\Observable;
+
+/**
+ * Custom error handler for PHP that allows for the execution of handlers
+ * and formatters for viewing and managing errors in development and production.
+ *
+ * @param bool $reset Resets the current error handler.
+ * @return BooBoo
+ * @see https://github.com/thephpleague/booboo/
+ */
+function error($reset = false) {
+    static $eh;
+    static $formatters = [
+        'html' => '\League\BooBoo\Formatter\HtmlFormatter',
+        'html_table' => '\League\BooBoo\Formatter\HtmlTableFormatter',
+        'json' => '\League\BooBoo\Formatter\JsonFormatter',
+        'cmd' => '\League\BooBoo\Formatter\CommandLineFormatter',
+        'null' => '\League\BooBoo\Formatter\NullFormatter'
+    ];
+
+    if ($eh instanceof BooBoo === false) {
+        $eh = new BooBoo([]);
+    }
+
+    if ($reset || empty($eh->getFormatters())) {
+        $eh->clearFormatters()
+           ->pushFormatter(new $formatters[config('error.formatter') ?? 'html_table'])
+           ->register();
+    }
+
+    return $eh;
+}
+
+error();
+config()->attach(new class implements ObserverInterface {
+    public function update(Observable $config) {
+        error(true);
+    }
+});
--- micro.php
+++ micro.php
@@ -3,5 +3,6 @@
 require __DIR__ . '/vendor/autoload.php';
 
 require_once 'lib/config.php';
+require_once 'lib/error.php';
 require_once 'lib/router.php';
 require_once 'lib/template.php';
