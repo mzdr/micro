c54f7a6ff1b2425182fcb212f9de9aca9641ab63
Sebastian Prein <hi@sebastianprein.com>
2018-01-31 19:24:30 +0100

    Use absolute paths with __DIR__

--- micro.php
+++ micro.php
@@ -1,9 +1,9 @@
 <?php
 
-require_once 'lib/config.php';
-require_once 'lib/error.php';
-require_once 'lib/database.php';
-require_once 'lib/router.php';
-require_once 'lib/template.php';
+require_once __DIR__ . '/lib/config.php';
+require_once __DIR__ . '/lib/error.php';
+require_once __DIR__ . '/lib/database.php';
+require_once __DIR__ . '/lib/router.php';
+require_once __DIR__ . '/lib/template.php';
 
-require_once 'init.php';
+require_once __DIR__ . '/init.php';
