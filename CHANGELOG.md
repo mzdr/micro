9388ea90daca37ceb513cde14e43f1adfc4a7b5b
Sebastian Prein <hi@sebastianprein.com>
2018-03-03 14:38:25 +0100

    Do not update error handler if new formatter is same as current one

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,4 +1,13 @@
+# [1.0.1]
+###### 2018-03-03
+
+Changing the error formatter in your configuration (e.g. `config()->set('error.formatter', 'json')`), should *only* trigger an update if the formatter *is different* to the currently set one.
+
+
 # 1.0.0
 ###### 2018-02-19
 
 You gotta start somewhere, right? 🌟
+
+
+[1.0.1]: https://github.com/mzdr/micro/compare/1.0.0...1.0.1
--- init.php
+++ init.php
@@ -5,26 +5,34 @@ namespace µ;
 use Gestalt\Util\ObserverInterface;
 use Gestalt\Util\Observable;
 
-$observer = new class implements ObserverInterface {
-    private $formatters = [
-        'html'       => '\League\BooBoo\Formatter\HtmlFormatter',
-        'html_table' => '\League\BooBoo\Formatter\HtmlTableFormatter',
-        'json'       => '\League\BooBoo\Formatter\JsonFormatter',
-        'cmd'        => '\League\BooBoo\Formatter\CommandLineFormatter',
-        'null'       => '\League\BooBoo\Formatter\NullFormatter'
-    ];
+config()->attach(
+    new class implements ObserverInterface {
+        private $formatters = [
+            'html'       => '\League\BooBoo\Formatter\HtmlFormatter',
+            'html_table' => '\League\BooBoo\Formatter\HtmlTableFormatter',
+            'json'       => '\League\BooBoo\Formatter\JsonFormatter',
+            'cmd'        => '\League\BooBoo\Formatter\CommandLineFormatter',
+            'null'       => '\League\BooBoo\Formatter\NullFormatter'
+        ];
 
-    public function update(Observable $config)
-    {
-        $formatter = config()->get('error.formatter');
+        private $currentFormatter = null;
 
-        if (isset($this->formatters[$formatter])) {
+        public function update(Observable $config)
+        {
+            $formatter = config()->get('error.formatter');
+
+            if (isset($this->formatters[$formatter]) === false || $formatter === $this->currentFormatter) {
+                return;
+            }
+
+            $this->currentFormatter = $formatter;
+
             error()->clearFormatters()
-                   ->pushFormatter(new $this->formatters[$formatter]);
+                   ->pushFormatter(
+                       new $this->formatters[$formatter]
+                   );
         }
     }
-};
-
-config()->attach($observer);
+);
 
 error();
