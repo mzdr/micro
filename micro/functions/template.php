d6916272aee667dc0d8277138779d9208274d294
Sebastian Prein <hi@sebastianprein.com>
2019-12-21 23:34:22 +0100

    Add folder support for Plates configuration

--- micro/functions/template.php
+++ micro/functions/template.php
@@ -33,6 +33,14 @@ function template(): Engine
     // otherwise take the default path as fallback
     $plates = new Engine($paths->views ?? $defaultViewsPath);
 
+    // If folders are given by configuration file, add all of them automatically.
+    // @see http://platesphp.com/v3/engine/folders/
+    if (is_array($paths->folders)) {
+        foreach ($paths->folders as $name => $folder) {
+            $plates->addFolder($name, ...(array) $folder);
+        }
+    }
+
     // If an assets path is given, also load the asset extension
     // @see http://platesphp.com/v3/extensions/asset/
     if (isset($paths->assets)) {
