5284edae44b0f38ef3c471962d348edb64a01ee0
Sebastian Prein <hi@sebastianprein.com>
2019-12-22 23:20:09 +0100

    Fix template folders not being added

--- micro/functions/template.php
+++ micro/functions/template.php
@@ -31,7 +31,7 @@ function template(): Engine
 
     // If folders are given by configuration file, add all of them automatically.
     // @see http://platesphp.com/v3/engine/folders/
-    if (is_array($paths->folders)) {
+    if ($paths->folders) {
         foreach ($paths->folders as $name => $folder) {
             $plates->addFolder($name, ...(array) $folder);
         }
